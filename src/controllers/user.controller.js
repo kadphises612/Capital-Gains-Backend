985f16b4be5fc4e888c9c8ddbfb29433b798d432
Kadphises <dev.abhi2902@gmail.com>
2025-12-24 15:21:35 +0530

    Cors and add Trade added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,3 +28,9 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ message: "User not found" });
+  res.json(user);
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createUser, getUsers } from "..//controllers/user.controller.js";
+import {
+  createUser,
+  getUsers,
+  getUserById,
+} from "..//controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
+router.get("/:id", getUserById);
 
 export default router;
