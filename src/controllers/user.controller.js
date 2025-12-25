e7069e06e3ab8fe2a1cfec122e972ee6ee3e9dcf
Kadphises <dev.abhi2902@gmail.com>
2025-12-25 14:51:23 +0530

    fetch summary too when user is fetched

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,7 +32,7 @@ export const getUsers = async (req, res) => {
 };
 
 export const getUserById = async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).populate("summary");
   if (!user) return res.status(404).json({ message: "User not found" });
   res.json(user);
 };
