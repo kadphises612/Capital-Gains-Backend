269a38f06bf5fcb93261bb41dae4637db810d693
Kadphises <dev.abhi2902@gmail.com>
2025-12-26 17:43:37 +0530

    user routes updated

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,13 +11,13 @@ import {
 
 const router = Router();
 
-router.post("/", createUser); // create user
-router.get("/", getUsers); // list users
-router.get("/:userId", getUser); // get one
-router.put("/:userId", updateUser); // update
-router.delete("/:userId", deleteUser); // delete
+router.post("/create", createUser); // create user
+router.get("/get-all", getUsers); // list users
+router.get("/get/:userId", getUser); // get one
+router.put("/update/:userId", updateUser); // update
+router.delete("/delete/:userId", deleteUser); // delete
 
-router.get("/:userId/summary", userSummaryHandler);
+router.get("/summary/:userId", userSummaryHandler);
 router.get("/summary/all", allUsersSummaryHandler);
 
 export default router;
